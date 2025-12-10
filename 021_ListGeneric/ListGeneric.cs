4481ffff4bb39afb9aad340ff542c5d1eee1f1e0
Danylo Lisnichuk <danilolisnicuk9@gmail.com>
2025-12-10 16:12:37 +0200

    Commit ListGeneric

---
+++ 021_ListGeneric/ListGeneric.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+
+namespace _021_ListGeneric
+{
+    class MyList<T> : IEnumerable
+    {
+        private T[] mass;
+
+
+        public int Count { get; set; }
+
+
+        public MyList()
+        {
+            mass = new T[10];
+        }
+
+        public MyList(int size = 10)
+        {
+            mass = new T[size];
+        }
+        public void PushBack(T value)
+        {
+            T[] newMass = new T[Count + 1];
+
+            for (int i = 0; i < Count; i++)
+                newMass[i] = mass[i];
+
+            newMass[Count] = value;
+            mass = newMass;
+
+            Count++;
+        }
+
+        public void PopBack()
+        {
+            if (Count == 0)
+            {
+                throw new Exception("Vector is empty");
+            }
+
+            Count--;
+        }
+
+        public void Insert(int index, T value)
+        {
+            if (index < 0 || index > Count)
+            {
+                throw new Exception("Index out of range");
+            }
+
+            if (Count == mass.Length)
+            {
+                T[] newMass = new T[Count + 1];
+                for (int i = 0; i < Count; i++)
+                {
+                    newMass[i] = mass[i];
+                }
+
+                mass = newMass;
+            }
+
+            for (int i = Count; i > index; i--)
+            {
+                mass[i] = mass[i - 1];
+            }
+
+            mass[index] = value;
+            Count++;
+        }
+
+        public void Erase(int index)
+        {
+            if (index < 0 || index >= Count)
+            {
+                throw new Exception("Index out of range");
+            }
+
+            for (int i = index; i < Count - 1; i++)
+            {
+                mass[i] = mass[i + 1];
+            }
+            Count--;
+        }
+
+        public bool isEmpty()
+        {
+            return Count == 0;
+        }
+
+        public T At(int index)
+        {
+            if (index >= 0 && index < Count)
+            {
+                return mass[index];
+            }
+            throw new Exception("Index out of range");
+        }
+
+        public void Reverse()
+        {
+            for (int i = 0; i < Count / 2; i++)
+            {
+                T temp = mass[i];
+                mass[i] = mass[Count - i - 1];
+                mass[Count - i - 1] = temp;
+            }
+        }
+
+        public void Clear()
+        {
+            Count = 0;
+        }
+
+        public override string ToString()
+        {
+            if (Count == 0)
+                return "List is Empty";
+
+            string result = "List array: ";
+            for (int i = 0; i < Count; i++)
+            {
+                result += mass[i] + ", ";
+            }
+            return result;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new UserCollectionEnumerator<T>(this);
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return At(index);
+            }
+            set
+            {
+                if (index < 0 || index >= Count)
+                {
+                    throw new Exception("Index out of range");
+                }
+                mass[index] = value;
+            }
+        }
+    }
+}
---
+++ 021_ListGeneric/Program.cs
@@ -0,0 +1,111 @@
+﻿using _021_ListGeneric;
+
+MyList<int> vector = new MyList<int>();
+
+while (true)
+{
+    Console.WriteLine("\n--- LIST MENU ---");
+    Console.WriteLine("1 - Add element to the end (PushBack)");
+    Console.WriteLine("2 - Remove element from the end (PopBack)");
+    Console.WriteLine("3 - Insert element by index (Insert)");
+    Console.WriteLine("4 - Erase element by index (Erase)");
+    Console.WriteLine("5 - Show element by index (At)");
+    Console.WriteLine("6 - Reverse list (Reverse)");
+    Console.WriteLine("7 - Clear list (Clear)");
+    Console.WriteLine("8 - Check if list is empty (Empty)");
+    Console.WriteLine("9 - Print list (ToString)");
+    Console.WriteLine("10 - Change element using indexer []");
+    Console.WriteLine("0 - Exit");
+    Console.Write("Your choice: ");
+
+    int choice = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+
+    try
+    {
+        switch (choice)
+        {
+            case 1:
+                Console.Write("Enter value to add: ");
+                int value = Convert.ToInt32(Console.ReadLine());
+                vector.PushBack(value);
+                Console.WriteLine("Element added.");
+                break;
+
+            case 2:
+                vector.PopBack();
+                break;
+
+            case 3:
+                Console.Write("Enter index for insertion: ");
+                int idxInsert = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter value: ");
+                int valInsert = Convert.ToInt32(Console.ReadLine());
+                vector.Insert(idxInsert, valInsert);
+                Console.WriteLine("Element inserted.");
+                break;
+
+            case 4:
+                Console.Write("Enter index for deletion: ");
+                int idxErase = Convert.ToInt32(Console.ReadLine());
+                vector.Erase(idxErase);
+                Console.WriteLine("Element erased.");
+                break;
+
+            case 5:
+                Console.Write("Enter index: ");
+                int idxAt = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Element[{idxAt}] = {vector.At(idxAt)}");
+                break;
+
+            case 6:
+                vector.Reverse();
+                Console.WriteLine("List reversed.");
+                break;
+
+            case 7:
+                vector.Clear();
+                Console.WriteLine("List cleared.");
+                break;
+
+            case 8:
+                Console.WriteLine(vector.isEmpty());
+                if (vector.isEmpty())
+                {
+                    Console.WriteLine("List is empty.");
+                }
+                else
+                {
+                    Console.WriteLine($"List is NOT empty. Count = {vector.Count}");
+                }
+                break;
+
+            case 9:
+                foreach (var item in vector)
+                {
+                    Console.WriteLine($"List: {item}, ");
+                }
+                break;
+
+            case 10:
+                Console.Write("Enter index: ");
+                int idxSet = Convert.ToInt32(Console.ReadLine());
+                Console.Write("New value: ");
+                int valSet = Convert.ToInt32(Console.ReadLine());
+                vector[idxSet] = valSet;
+                Console.WriteLine("Element changed using indexer.");
+                break;
+
+            case 0:
+                return;
+
+            default:
+                Console.WriteLine("Invalid menu item.");
+                break;
+        }
+    }
+    catch (Exception exception)
+    {
+        Console.WriteLine("Error: " + exception.Message);
+    }
+}
